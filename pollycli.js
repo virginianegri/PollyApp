27bfd013fb2b06730585e6c7b8ab7819498cd496
Mohsin Khan <mohsin@zaavya.com>
2020-01-29 20:59:17 +0500

    Modified cli pollycli to handle config creation

--- pollycli.js
+++ pollycli.js
@@ -26,6 +26,13 @@ const argv = require('yargs')
     })
   }, function (argv) {
   })
+  .command('createconfig [pathsharedconfig]', 'welcome to pollycli', (yargs) => {
+    yargs.positional('pathsharedconfig', {
+      type: 'string',
+      describe: 'path of shared config folder!'
+    })
+  }, function (argv) {
+  })
   .command('multitext', 'welcome to pollycli', function (argv) {
   })
   .help()
@@ -36,25 +43,40 @@ const argv = require('yargs')
  */
 async function cli_init(args) {
   // console.log('From require: ', args);
+  if (args._[0] == 'createconfig') {
+    let config = {}
+    config.shared_folder_path = args.pathsharedconfig
 
-  let finalConfig = await checkConfig('./config.json');
+    let data = JSON.stringify(config);
+    Fs.writeFileSync('./config.json', data);
+  }
+  else {
+    try {
+      let finalConfig = await checkConfig('./config.json');
 
-  await authenticate(finalConfig.config, finalConfig.sharedConfig.aws_pool_id);
-  // console.log(args._[0]);
+      await authenticate(finalConfig.config, finalConfig.sharedConfig.aws_pool_id);
+      // console.log(args._[0]);
 
-  if (args._[0] == 'singletext') {
-      let filePath = `${finalConfig.config.shared_folder_path}/${finalConfig.sharedConfig.text_folder}/${args.filename}.txt`
-      let textToSpeech = await readFile(filePath);
-      let pollyParams = getPollyParams(textToSpeech, finalConfig.sharedConfig);
+      if (args._[0] == 'singletext') {
+          let filePath = `${finalConfig.config.shared_folder_path}/${finalConfig.sharedConfig.text_folder}/${args.filename}.txt`
+          let textToSpeech = await readFile(filePath);
+          let pollyParams = getPollyParams(textToSpeech, finalConfig.sharedConfig);
 
-      await generateAudio(pollyParams, args.filename, `${finalConfig.config.shared_folder_path}/${finalConfig.sharedConfig.audio_folder}`)
+          await generateAudio(pollyParams, args.filename, `${finalConfig.config.shared_folder_path}/${finalConfig.sharedConfig.audio_folder}`)
 
-  } else if (args._[0] == 'multitext') {
+      } else if (args._[0] == 'multitext') {
           let filePath = `${finalConfig.config.shared_folder_path}/${finalConfig.sharedConfig.text_folder}`;
           const fileNames = Fs.readdirSync(filePath);
           await generateAllAudios(fileNames, filePath, finalConfig);
-  } else if (args._[0] == 'singlepptx') {
-      await processPPTXFile(`${args.filename}.pptx`);
+      } else if (args._[0] == 'singlepptx') {
+          await processPPTXFile(`${args.filename}.pptx`);
+      } else {
+          console.log('Please give a correct command! Refer to --help for right commands!');
+      }
+    }
+    catch(error) {
+      console.log('Please use createconfig command to create config');
+    }
   }
 }
 
